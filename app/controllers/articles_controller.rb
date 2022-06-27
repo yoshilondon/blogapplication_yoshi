79f3c2dbcd6d5f4f808027786e8435e2a7ca8f46
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-06-27 20:58:11 +0100

    flashメッセージに表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to article_path(@article)
+      redirect_to article_path(@article), notice: '保存できました'
     else
+      flash.now[:error] = '保存に失敗しました'
       render :new
     end
     # @article.save : データベースに値を保存する
