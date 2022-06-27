6f7efe052dd51919bc5a5bc86a1de3ec7d7b92ce
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-06-27 16:17:51 +0100

    createアクションを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,20 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+    # @article.save : データベースに値を保存する
+  end
+
+  private
+  def article_params
+  # ホームから投稿されたデータの中からタイトルとコンテンツを抜き出す。
+    params.require(:article).permit(:title, :content)
+  end
 end
