4dbe190c98d652f5a4f3aa8d5e7f9f13306daf12
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-07-22 16:21:16 +0100

    自分の記事に対してアクションボタンを表示するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :articles, dependent: :destroy
+
+  def has_written?(article)
+    articls.exits?(id: article.id)
+  end
 end
