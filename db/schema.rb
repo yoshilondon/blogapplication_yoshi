1197e3a25c2e15d3e2d43123dcf233fb6050c7b0
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-07-22 15:29:30 +0100

    add_reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_17_121921) do
+ActiveRecord::Schema.define(version: 2022_07_22_141936) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
