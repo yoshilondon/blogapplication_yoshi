9a4f5d601b8c9118f2bc410cf6d92222a9b0a20d
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-07-27 21:14:19 +0100

    no message

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,12 +2,12 @@
 #
 # Table name: articles
 #
-#  id         :bigint           not null, primary key
+#  id         :integer          not null, primary key
 #  content    :text             not null
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :bigint           not null
+#  user_id    :integer          not null
 #
 # Indexes
 #
--- test/fixtures/comments.yml
+++ test/fixtures/comments.yml
@@ -2,11 +2,11 @@
 #
 # Table name: comments
 #
-#  id         :bigint           not null, primary key
+#  id         :integer          not null, primary key
 #  content    :text             not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  article_id :bigint           not null
+#  article_id :integer          not null
 #
 # Indexes
 #
