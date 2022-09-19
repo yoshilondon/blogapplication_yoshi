1d11ceee1dc843aa020e1053767b7c3f42c1630f
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-09-19 15:52:23 +0100

    フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   end
   # resourcesはいろいろなurlをrailsが自動で作ってくれる
 
-  resources :accounts, only: [:show]
+  resources :accounts, only: [:show] do
+    resources :follows, only: [:create]
+  end
 
   resource :profile, only: [:show, :edit, :update]
   resources :favorites, only: [:index]
