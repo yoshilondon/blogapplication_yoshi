3bc8a87cea16aea22e26ca3da3c54763aab83abc
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-09-25 20:10:11 +0100

    リファクタリング

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -12,6 +12,21 @@ const handleHeartDisplay = (hasLiked) => {
   }
 }
 
+// show-comment-formをクリックすると、hiddenがクラス名に追加されて表示が消える。
+// text-areaのhiddenがクラス名から外されて表示される
+const handleCommentForm = () => {
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment-text-area').removeClass('hidden')
+  })
+}
+
+const appendNewComment = (comment) => {
+  $('.comments-container').append(
+    `<div class="article_comment"><p>${comment.content}</p></div>`
+  )
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
@@ -20,17 +35,11 @@ document.addEventListener('DOMContentLoaded', () => {
     .then((response) => {
       const comments = response.data
       comments.forEach((comment) => {
-        $('.comments-container').append(
-          `<div class="article_comment"><p>${comment.content}</p></div>`
-        )
+        appendNewComment(comment)
       })
     })
-  // show-comment-formをクリックすると、hiddenがクラス名に追加されて表示が消える。
-  // text-areaのhiddenがクラス名から外されて表示される
-  $('.show-comment-form').on('click', () => {
-    $('.show-comment-form').addClass('hidden')
-    $('.comment-text-area').removeClass('hidden')
-  })
+
+  handleCommentForm()  
 
   $('.add-comment-button').on('click', () => {
     // comment_contentの値を取得
@@ -43,9 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
       })
         .then((res) => {
           const comment = res.data
-          $('.comments-container').append(
-            `<div class="article_comment"><p>${comment.content}</p></div>`
-          )
+          appendNewComment(comment)
           $('#comment_content').val('')
         })
     }
