e85ee404d28bcede796c7e51a8b50b965b79b342
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-10-01 19:17:04 +0100

    コールバックでメール送付

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -2,6 +2,6 @@ class RelationshipMailer < ApplicationMailer
   def new_follower(user, follower)
     @user = user
     @follower = follower
-    mail to: user.email, subject: ' 【お知らせ】フォローされました'
+    mail to: user.email, subject: '【お知らせ】フォローされました'
   end
 end
