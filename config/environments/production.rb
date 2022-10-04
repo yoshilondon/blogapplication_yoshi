b1dbdde77ff5c2f1f555c5e741f897c23a2ed128
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-10-04 22:31:39 +0100

    Update application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliberies = true
+  config.action_mailer.perform_deliveries = true
   host = 'blog-app-yoshi.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }  
   ActionMailer::Base.smtp_settings = {
